3a8d5b149602f8ba82dd0de30a365d3cc802fa41
Nat Troyer <nathanaelatroyer@gmail.com>
2020-10-07 13:33:57 -0400

    questions wiring

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+// todo - eventually, we will want to load the questions from a database in the backend, maybe...
+import * as questions from '../questions/questions.json';
+
+interface Question {
+  question: String;
+  topic: String;
+}
+
+interface Player {
+  name: string;
+  question: Question;
+  answer: string;
+}
 
 @Component({
   selector: 'app-game',
@@ -6,10 +19,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  questions: Question[] = questions.default;
+
+  public currentQuestionObj: Question;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  showQuestion() {
+    const randomQuestion = this.questions[Math.floor(Math.random() * this.questions.length)];
+    this.currentQuestionObj = randomQuestion;
+  }
+
+  hasQuestion() {
+    return typeof this.currentQuestionObj !== 'undefined';
+  }
+
+  newPlayer() {
+    console.log('newPlayer');
+  }
+
+  finish() {
+    console.log('finish!!!');
+  }
+
 
 }
