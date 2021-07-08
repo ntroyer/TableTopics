73c705dc9f07232bb36233414543e4f891c9758f
Nat Troyer <nathanaelatroyer@gmail.com>
2021-07-08 10:23:06 -0400

    all the basic karma tests now pass

--- src/app/game/game.component.spec.ts
+++ src/app/game/game.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameComponent } from './game.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('GameComponent', () => {
   let component: GameComponent;
@@ -8,7 +10,8 @@ describe('GameComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GameComponent ]
+      declarations: [ GameComponent ],
+      imports: [ RouterTestingModule, BrowserAnimationsModule ]
     })
     .compileComponents();
   }));
--- src/app/game/timer/timer.component.spec.ts
+++ src/app/game/timer/timer.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimerComponent } from './timer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('TimerComponent', () => {
   let component: TimerComponent;
@@ -8,7 +9,8 @@ describe('TimerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TimerComponent ]
+      declarations: [ TimerComponent ],
+      imports: [ BrowserAnimationsModule ]
     })
     .compileComponents();
   }));
